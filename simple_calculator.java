76c0776024fa57046fafe5dcde731bed7fbf2c71
Karabo <78500285+karabomasoeu@users.noreply.github.com>
2026-01-07 22:30:52 +0200

    Update simple_calculator.java

--- simple_calculator.java
+++ simple_calculator.java
@@ -1,1 +1,11 @@
-
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        
+        System.out.println("=== Simple Calculator ===");
+        System.out.println("Enter first number:");
+        double num1 = sc.nextDouble();
+        
+        System.out.println("Enter operator (+, -, *, /):");
+        char operator = sc.next().charAt(0);
+       
