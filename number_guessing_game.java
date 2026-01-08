ee605233b3bc0c0a7cd4450dd2049c977604715e
Karabo <78500285+karabomasoeu@users.noreply.github.com>
2026-01-08 22:23:58 +0200

    Update number_guessing_game.java

--- number_guessing_game.java
+++ number_guessing_game.java
@@ -1,1 +1,9 @@
+import java.util.Random;
+import java.util.Scanner;
 
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int targetNumber = random.nextInt(100) + 1;
+        int guess = 0;
+        int attempts = 0;
